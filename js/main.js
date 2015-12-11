bc9e1e65bbff9d255a51ae2a0422318291d94ccf
Lynn Tran <carlpinto@Lynns-iMac.local>
2015-12-10 23:17:57 -0500

    collision stuck

--- js/main.js
+++ js/main.js
@@ -2,6 +2,22 @@ $(document).ready(function(){
 	$(function() {
     $( ".ball" ).draggable();
   });
-	
+var minutes = 1;
+var seconds = minutes * 60;
 
+function convertIntToTime (num) {
+    var mins = Math.floor(num/60);
+    var secs = num % 60;
+    var timerOutput = (mins < 10 ? "0" : "" ) + mins + ":" + (secs < 10 ? "0" : "" ) + secs;
+    return(timerOutput);
+}
+
+var countdown = setInterval(function() {
+    var current = convertIntToTime(seconds); 
+    $('.timer').html(current); 
+    if(seconds == 0) { 
+        clearInterval(countdown); 
+    }
+    seconds--; 
+}, 1000);
 });
