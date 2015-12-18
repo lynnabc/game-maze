9a381cec9aee6debd6121825575d7a72148f6ab9
Lynn Tran <carlpinto@Lynns-iMac.local>
2015-12-18 00:30:34 -0500

    T.T

--- js/main.js
+++ js/main.js
@@ -1,42 +1,48 @@
-$document.ready(function(){
+$(document).ready(function(){
 
-$('.play').click(function(){
-    $(this).parent('#welcome').fadeOut(500);
-});
+    $('#maze').css({'cursor': 'crosshair'});
+    $('#retry').replaceWith(function() {
+     return $("<a></a>").attr("href","index.html").append($(this).contents());
+   });
 
-$('.play').click(function(){
-    $(this).parent('#boundary').fadeIn(500);
-});
+       var minutes = 1;
+    var seconds = minutes * 60;
 
-$('.boundary').css('cursor','crosshair');
+    function convertIntToTime (num) {
+        var mins = Math.floor(num/60);
+        var secs = num % 60;
+        var timerOutput = (mins < 10 ? "0" : "" ) + mins + ":" + (secs < 10 ? "0" : "" ) + secs;
+        return(timerOutput);
+    }
 
-
-
-var minutes = 1;
-var seconds = minutes * 60;
-
-function convertIntToTime (num) {
-    var mins = Math.floor(num/60);
-    var secs = num % 60;
-    var timerOutput = (mins < 10 ? "0" : "" ) + mins + ":" + (secs < 10 ? "0" : "" ) + secs;
-    return(timerOutput);
-}
-
-var countdown = setInterval(function() {
-    var current = convertIntToTime(seconds); 
-    $('.timer').html(current); 
-    if(seconds == 0) { 
+    var countdown = setInterval(function() {
+        var current = convertIntToTime(seconds); 
+        $('.timer').html(current); 
+        if(seconds == 0) { 
         clearInterval(countdown); 
+        }
+        seconds--; 
+    }, 1000);var minutes = 1;
+    var seconds = minutes * 60;
+
+    function convertIntToTime (num) {
+        var mins = Math.floor(num/60);
+        var secs = num % 60;
+        var timerOutput = (mins < 10 ? "0" : "" ) + mins + ":" + (secs < 10 ? "0" : "" ) + secs;
+        return(timerOutput);
     }
-    seconds--; 
-}, 1000);
-function(){
-    if (seconds ==0) {
-         $('#GameOver').fadeIn();
-            $('#retry').click(function () {
-                $('#GameOver').fadeOut();
-            });
-    }
-};
+
+    var countdown = setInterval(function() {
+        var current = convertIntToTime(seconds); 
+        $('.timer').html(current); 
+        if(seconds == 0){ 
+        clearInterval(countdown); 
+        setTimeout("window.location.href='lose.html';");
+        }
+        seconds--; 
+    }, 1000);
+    
+
 });
+
 
